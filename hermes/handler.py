cb6830c6470425641fe7c9dfec4522fbc099e6c8
diegoalrv <d.alexis.ramirez@gmail.com>
2024-04-30 17:07:57 -0400

    added functions to query data from an exact project

--- hermes/handler.py
+++ hermes/handler.py
@@ -12,14 +12,19 @@ from shapely.geometry import Point, LineString, Polygon
 class Handler:
     def __init__(self):
         self.server_address = os.getenv('server_address', 'http://localhost:8000')
-        self.request_data_endpoint = os.getenv('request_data_endpoint', 'api')
-        self.id_network = os.getenv('id_roadnetwork', 1)
         self.project_name = os.getenv('project_name', '')
+        self.set_project_id()
     
     @staticmethod
     def generate_unique_code(strings):
         text = ''.join(strings)
         return hashlib.sha256(text.encode()).hexdigest()
+    
+    def set_project_id(self):
+        url = f'{self.server_address}/interactive/project/get-project-id-by-name/?name={self.project_name}'
+        r = requests.get(url)
+        self.project_id = r.json()['project_id']
+        pass
 
     def post_data(self, endpoint='', data=None, headers={'Content-Type': 'application/json'}):
         endpoint = endpoint if endpoint else f'{self.server_address}/urban-indicators/indicatordata/upload_to_table/'
@@ -64,41 +69,60 @@ class Handler:
         return data
 
     # Methods to load data from API
+    # http://192.168.31.120:8001/interactive/project/1/get-data/{which_one?}/
     
-    def load_amenities(self):
-        amenities = None
-        endpoint = f'{self.server_address}/{self.request_data_endpoint}/amenity/'
-        response = requests.get(endpoint)
-        data = response.json()
-        
+    def geojson_to_geodataframe(self, data_json):
         geometries = []
         properties = []
-        for feature in data['features']:
+        for feature in data_json['features']:
             geometries.append(wkt.loads(feature['geometry'].split(';')[-1]))
             properties.append(feature['properties'])
+        return gpd.GeoDataFrame(properties, geometry=geometries)
 
-        amenities = gpd.GeoDataFrame(properties, geometry=geometries)
+    def load_amenities(self):
+        amenities = None
+        endpoint = f'{self.server_address}/interactive/project/{self.project_id}/get-data/amenities/'
+        response = requests.get(endpoint)
+        data_json = response.json()
+        amenities = self.geojson_to_geodataframe(data_json=data_json)
         amenities.drop(columns=['tags'], inplace=True)
         return amenities
 
-    def load_area_of_interest(self, id=2):
+    def load_area_of_interest(self, id=1):
         area_of_interest = None
-        endpoint = f'{self.server_address}/{self.request_data_endpoint}/areaofinterest/{id}/'
+        endpoint = f'{self.server_address}/interactive/project/{self.project_id}/area-of-interest/'
         response = requests.get(endpoint)
         data = response.json()
         geometries= [wkt.loads(data['geometry'].split(';')[-1])]
         properties= [data['properties']]
-
         area_of_interest = gpd.GeoDataFrame(properties, geometry=geometries)
         area_of_interest = area_of_interest.set_crs(4326)
         return area_of_interest
-
+    
+    def load_aggregation_polys(self, dist_type=None, level=None):
+        endpoint = f'{self.server_address}/interactive/project/{self.project_id}/discrete-distributions/?dist_type={dist_type}&level={level}'
+        response = requests.get(endpoint)
+        data_json = response.json()
+        unit = self.geojson_to_geodataframe(data_json=data_json)
+        unit.set_crs(4326, inplace=True)
+        return unit
+    
+    def load_green_areas(self):
+        endpoint = f'{self.server_address}/interactive/project/{self.project_id}/get-data/green_areas/'
+        response = requests.get(endpoint)
+        data_json = response.json()
+        green_areas = self.geojson_to_geodataframe(data_json=data_json)
+        green_areas.set_crs(4326, inplace=True)
+        return green_areas
+        
     def load_network(self, id_network=None):
         if id_network is None:
             id_network = self.id_network
-        filename = f'/app/tmp/net_{id_network}.h5'
+        download_path = '/app/tmp'
+        os.makedirs(download_path, exist_ok=True)
+        filename = os.path.join(download_path, f'net_{id_network}.h5')
         if not os.path.exists(filename):
-            endpoint = f'{self.server_address}/{self.request_data_endpoint}/roadnetwork/{id_network}/serve_h5_file/'
+            endpoint = f'{self.server_address}/api/roadnetwork/{id_network}/serve_h5_file/'
             print(endpoint)
             response = requests.get(endpoint)
             if response.status_code == 200:
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-version = "0.1.5.6"
+version = "0.1.5.7"
 
 setup(
     name='clbb-hermes',
